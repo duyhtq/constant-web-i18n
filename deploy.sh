f397bf26f59f1c24f872ac9a1a6bdc27a440410f
0xKoddoelo <0xKoddoelo@gmail.com>
2019-02-20 13:19:38 +0700

    Origin Feature

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # export APP_NAME=$1 export CROWDIN_API=$2
+hasChange=$false
 echo "If Submodule has any new update"
 if [[ `git status --porcelain` ]]; then
   echo "Has some new update from frontend repo"
   git add .
   git commit -m "Pull newest version from frontend git repo"
+  hasChange=$true
 else
   echo "No Change from frontend repo"
 fi
@@ -15,6 +17,7 @@ if [[ `git status --porcelain` ]]; then
   echo "Has new translation need to send to crowdin"
   git add .
   git commit -m "Commit for pushing to crowdin"
+  hasChange=$true
   yarn send
 else
   echo "No thing new to push to crowdin"
@@ -26,8 +29,15 @@ if [[ `git status --porcelain` ]]; then
   echo "Download from crowdin and has some new changes"
   git add .
   git commit -m "Update new version for translation"
+  hasChange=$true
 else
   echo "No Change"
 fi
 # git push origin integrate/CI
+if [ $hasChange ]
+then
+  echo "HAS CHANGE TO PUSH"
+else
+  echo "NOTHING TO PUSH"
+fi
 echo "Done"

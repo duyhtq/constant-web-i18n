5f4bbbef52d86736713225c2e9749f1f572fa69b
khoatran <43737100+0xKoddoelo@users.noreply.github.com>
2019-04-01 15:13:35 +0700

    Update i18n.js
    add lang

--- i18n.js
+++ i18n.js
@@ -1,7 +1,8 @@
 
 const en = require('./i18n/i18n/en.json');
 const vi = require('./i18n/i18n/vi.json');
+const enUS = require('./i18n/i18n/enUS.json');
 
 module.exports = {
-  en, vi
+  en, vi, enUS
 };

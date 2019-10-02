7c9dfa3e1904944401a9fc0cdc48aa685efb5646
khoatran <43737100+0xKoddoelo@users.noreply.github.com>
2019-10-02 11:32:19 +0700

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -2,7 +2,8 @@
 const en = require('./i18n/i18n/en.json');
 const vi = require('./i18n/i18n/vi.json');
 const enUS = require('./i18n/i18n/enUS.json');
+const enGB = require('./i18n/i18n/enGB.json');
 
 module.exports = {
-  en, vi, enUS
+  en, vi, enUS, enGB
 };

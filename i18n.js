ef042156200a9547a2ea9cb9af0480102df43ed0
0xKoddoelo <0xKoddoelo@gmail.com>
2019-12-02 08:29:09 +0700

    updat lang

--- i18n.js
+++ i18n.js
@@ -2,7 +2,8 @@
 const en = require('./i18n/i18n/en.json');
 const vi = require('./i18n/i18n/vi.json');
 const enUS = require('./i18n/i18n/enUS.json');
-
+const zhCN = require('./i18n/i18n/zhCN.json');
+const zhTW = require('./i18n/i18n/zhTW.json');
 module.exports = {
-  en, vi, enUS
+  en, vi, enUS, zhCN, zhTW
 };

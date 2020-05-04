7c1950e592ab6b4756e9f48a189ee4c918a5a02c
0xmegalodon <dung.thanh@autonomous.nyc>
2020-05-04 18:26:36 +0700

    -: remove localize

--- i18n.js
+++ i18n.js
@@ -1,9 +1,9 @@
 
-const en = require('./i18n/i18n/en.json');
-const vi = require('./i18n/i18n/vi.json');
+// const en = require('./i18n/i18n/en.json');
+// const vi = require('./i18n/i18n/vi.json');
 const enUS = require('./i18n/i18n/enUS.json');
-const zhCN = require('./i18n/i18n/zhCN.json');
-const zhTW = require('./i18n/i18n/zhTW.json');
+// const zhCN = require('./i18n/i18n/zhCN.json');
+// const zhTW = require('./i18n/i18n/zhTW.json');
 module.exports = {
-  en, vi, enUS, zhCN, zhTW
+  enUS
 };
